1ce8a03f734487cb1f8abb4a857e558a102e4c71
Michael Reinart <M_reinhart@ymail.com>
2016-03-25 00:09:37 -0400

    Added files via upload

---
+++ HelloPrinter.java
@@ -0,0 +1,22 @@
+/*
+ * Description: My First program
+ * Author: Mrs. Fearrington
+ * Contributors: Michael Reinhart
+ * Date:  8/28/13
+ * 
+ */
+
+
+
+public class HelloPrinter
+{
+   public static void main(String[] args)
+   {
+      System.out.print("Hello, World! ");
+      System.out.println("Hi Michael");
+      System.out.println("I am in this class");
+  
+     
+   }
+}
+ 
---
+++ MyClass.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class MyClass here.
+ * 
+ *  Michael Reinhart 
+ * Date-8/28/2015
+ */
+public class MyClass
+{
+    public static void main(String[] args)  {
+    System.out.println("Computer Science");
+    System.out.print("This class in the 3rd period");
+    System.out.print(" and is interesting");
+    System.out.println("I have english class next");
+    
+    }   
+}
+
+
+
+
+
+
+
+
+
+
+
